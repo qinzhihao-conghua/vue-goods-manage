4c6ede75b8e8163f48bb2816cd48f0c187f152f5
qinzhihao-conghua <1043419697@qq.com>
2020-12-08 21:00:38 +0800

    订单列表和数据报表

--- src/components/orders/orders.vue
+++ src/components/orders/orders.vue
@@ -1,17 +1,120 @@
 <template>
-  <div>订单列表</div>
+  <el-card class="box-card">
+    <zh-bread :level1="'订单管理'" :level2="'订单列表'"></zh-bread>
+    <el-table :data="orderList" class="table-container">
+      <el-table-column type="index" label="#" width="60"> </el-table-column>
+      <el-table-column prop="order_number" label="订单编号"> </el-table-column>
+      <el-table-column prop="order_price" label="订单价格(元)"> </el-table-column>
+      <el-table-column label="是否付款">
+        <template slot-scope="scope">
+          <el-tag type="danger" size="mini" v-if="scope.row.pay_status">未付款</el-tag>
+          <el-tag type="success" size="mini" v-else>已付款</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column prop="is_send" label="是否发货"> </el-table-column>
+      <el-table-column label="下单时间">
+        <template slot-scope="scope">
+          {{ scope.row.create_time | frmDate }}
+        </template>
+      </el-table-column>
+      <el-table-column prop="address" label="操作">
+        <template slot-scope="scope">
+          <el-button type="primary" @click="openEditWindow(scope.row)" size="mini" plain icon="el-icon-edit" circle>
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="pagenum" :page-sizes="[2, 4, 6, 8]" :page-size="pagesize" layout="total, sizes, prev, pager, next, jumper" :total="total">
+    </el-pagination>
+    <el-dialog title="编辑地址" :visible.sync="editWindow">
+      <el-form>
+        <el-form-item label="省市区/县" :label-width="'100px'">
+          <el-cascader v-model="selectedOptions" :options="options" :props="defaultPro" clearable @change="handleChange">
+          </el-cascader>
+        </el-form-item>
+        <el-form-item label="详细地址" :label-width="'100px'">
+          <el-input v-model="address" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="editWindow = false">取 消</el-button>
+        <el-button type="primary" @click="saveAddress()">确 定</el-button>
+      </div>
+    </el-dialog>
+  </el-card>
 </template>
 
 <script>
+import citydata from './citydata.js';
 export default {
   name: 'orders',
   components: {},
   props: {},
   data() {
     return {
+      orderList: [],
+      pagenum: 1,
+      pagesize: 15,
+      total: -1,
+      query: '',
+      editWindow: false,
+      options: [],
+      selectedOptions: [],
+      defaultPro: {
+        expandTrigger: 'hover',
+        label: 'label',
+        value: 'value',
+        children: 'children'
+      },
+      address: ''
     };
+  },
+  created() {
+    this.getOrderList();
+  },
+  methods: {
+    openEditWindow(dataItem) {
+      this.editWindow = true;
+      this.options = citydata;
+    },
+    async getOrderList() {
+      // user_id
+      // pay_status
+      // is_send
+      // order_fapiao_title
+      // order_fapiao_company
+      // order_fapiao_content
+      // consignee_addr
+      const res = await this.$axios.get(`orders?query=${this.query}&pagenum=${this.pagenum}&pagesize=${this.pagesize}`);
+      if (res.data.meta.status === 200) {
+        this.orderList = res.data.data.goods;
+        this.total = res.data.data.total;
+      } else {
+        this.$message.error(res.data.meta.msg);
+      }
+    },
+    async saveAddress() {
+      this.editWindow = false;
+    },
+    handleChange() { },
+    handleSizeChange(value) {
+      this.pagenum = 1;
+      this.pagesize = value;
+      // this.getGoodsList();
+    },
+    handleCurrentChange(value) {
+      this.pagenum = value;
+      // this.getGoodsList();
+    }
   }
 };
 </script>
-<style lang="css" scoped>
+<style lang="scss" scoped>
+.box-card {
+  min-height: 100%;
+  .table-container {
+    margin-top: 16px;
+    width: 100%;
+  }
+}
 </style>
--- src/components/reports/reports.vue
+++ src/components/reports/reports.vue
@@ -1,17 +1,70 @@
 <template>
-  <div>数据报表</div>
+  <el-card class="box-card">
+    <zh-bread :level1="'数据统计'" :level2="'数据报表'"></zh-bread>
+    <div id="main" style="width: 750px;height:400px;margin-top:16px;"></div>
+  </el-card>
 </template>
 
 <script>
+import echarts from 'echarts'
 export default {
   name: 'reports',
   components: {},
   props: {},
   data() {
     return {
+      // 需要合并的数据
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            type: 'category',
+            boundaryGap: false,
+            data: []
+          }
+        ],
+        yAxis: [
+          { type: 'value' }
+        ],
+        series: [],
+
+      }
     };
-  }
+  },
+  // 此时,页面上的元素,已经被渲染完毕了
+  async mounted() {
+    // 3.基于准备好的dom，初始化echarts实例
+    const myChart = echarts.init(document.getElementById('main'));
+    const res = await this.$axios.get('reports/type/1');
+    if (res.data.meta.status !== 200) return this.$message('获取折线图数据失败!');
+    const option1 = res.data.data;
+    console.log('echarts', option1);
+    this.options.series = [...option1.series]
+    this.options.xAxis = [...option1.xAxis]
+    // 5.展示数据
+    myChart.setOption(this.options);
+  },
 };
 </script>
-<style lang="css" scoped>
+<style lang="scss" scoped>
+.box-card {
+  min-height: 100%;
+}
 </style>
